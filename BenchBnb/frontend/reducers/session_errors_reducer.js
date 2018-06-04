d084ca02f5eb3e526d908272c4230827ef77fbb7
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-04 14:59:18 -0700

    finish session errors reducer

---
+++ BenchBnb/frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,18 @@
+import {
+  RECEIVE_ERRORS,
+  RECEIVE_CURRENT_USER
+} from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const SessionErrorsReducer = (oldState = [], action) => {
+  Object.freeze(oldState);
+  switch(action.type){
+    case RECEIVE_ERRORS:
+    return action.errors;
+    case RECEIVE_CURRENT_USER:
+    return [];
+    default: return oldState;
+  }
+};
+
+export default SessionErrorsReducer;
